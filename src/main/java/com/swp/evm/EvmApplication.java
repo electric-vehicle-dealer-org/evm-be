b47c2d8ad73cacd14c04c03a41af92371de7f98d
DanielHieu <47274268+DanielHieu@users.noreply.github.com>
2025-10-08 14:37:28 +0700

    chore: initialize spring boot project structure

---
+++ src/main/java/com/swp/evm/EvmApplication.java
@@ -0,0 +1,14 @@
+package com.swp.evm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvmApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/swp/evm/EvmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swp.evm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
